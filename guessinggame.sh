fe45d53efd26c6d5b929c8bd53ca51937139e7fe
Siham Bouharfe <134154914+Siham-Bouharfe@users.noreply.github.com>
2025-01-23 13:40:59 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,20 @@
+#!/usr/bin/env bash
 
+guess_files() {
+    local file_count=$(ls -1 | wc -l) 
+    local guess=-1
+
+    echo "Guess how many files are in the current directory:"
+    while [[ $guess -ne $file_count ]]; do
+        read -p "Enter your guess: " guess
+        if [[ $guess -lt $file_count ]]; then
+            echo "Your guess is too low. Try again."
+        elif [[ $guess -gt $file_count ]]; then
+            echo "Your guess is too high. Try again."
+        else
+            echo "Congratulations! You guessed correctly."
+        fi
+    done
+}
+
+guess_files
